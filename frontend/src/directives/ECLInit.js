d1436e27cfe798bc4de2c9f84878e87cec0ce181
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-11-08 17:53:44 +0200

    Add directive to init ECL components

---
+++ frontend/src/directives/ECLInit.js
@@ -0,0 +1,77 @@
+/**
+ * Custom directive to initialize an ECL component when it is added to the DOM.
+ * Also destroys the component when the root element is unmounted from the DOM.
+ *
+ * Example usage:
+ *
+ * <div
+ *   v-ecl-init
+ *   role="alert"
+ *   class="ecl-message ecl-message--info"
+ *   data-ecl-message
+ *   data-ecl-auto-init="Message"
+ * >
+ *   ... Message component ...
+ * </div>
+ *
+ * Makes it much easier to manage components as they are dynamically added/removed
+ * from the app.
+ *
+ * See also documentation for the ECL component library here:
+ *
+ *   https://ec.europa.eu/component-library/ec/getting-started/
+ *
+ */
+
+export default {
+  // called when the bound element's parent component
+  // and all its children are mounted.
+  mounted(el) {
+    // Requires attribute data-ecl-auto-init="COMPONENT"
+    const componentName = el.dataset.eclAutoInit;
+
+    if (el.dataset.eclAutoInitialized === "true") {
+      console.warn("Component already initialized for:", el);
+      return;
+    }
+
+    if (!componentName || !window.ECL[componentName]) {
+      console.warn(
+        `Invalid or missing data-ecl-auto-init value: ${componentName}`,
+        el
+      );
+      return;
+    }
+
+    window.ECL[componentName].autoInit(el);
+  },
+  // called when the parent component is unmounted
+  unmounted(el) {
+    // Requires attribute data-ecl-auto-init="COMPONENT"
+    const componentName = el.dataset.eclAutoInit;
+
+    if (el.dataset.eclAutoInitialized !== "true") {
+      console.warn("Component not yet initialized for:", el);
+      return;
+    }
+
+    if (!componentName || !window.ECL[componentName]) {
+      console.warn(
+        `Invalid or missing data-ecl-auto-init value: ${componentName}`,
+        el
+      );
+      return;
+    }
+
+    // ECL autoInit attaches the created component to the DOM Element.
+    // E.g. for the 'Message' component the ECLMessage is created.
+    const component = el[`ECL${componentName}`];
+
+    if (!component) {
+      console.warn(`Component '${componentName}' not found for:`, el);
+      return;
+    }
+
+    component.destroy();
+  },
+};
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,13 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import ECLInit from "@/directives/ECLInit";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.directive("ecl-init", ECLInit);
 
 app.mount("#app");
