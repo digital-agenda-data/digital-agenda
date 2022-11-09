aeb2b944576f5ff4753e4e8c51b1bf985103e815
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-11-09 11:19:32 +0200

    Add ECL Search Form component.

---
+++ frontend/src/components/ecl/forms/EclSearchForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <form class="ecl-search-form" role="search" :action="action" :method="method">
+    <div class="ecl-form-group">
+      <div v-if="helpText" class="ecl-help-block">
+        {{ helpText }}
+      </div>
+      <label :for="inputName" class="ecl-form-label ecl-search-form__label">
+        Search
+      </label>
+      <input
+        :name="inputName"
+        type="search"
+        class="ecl-text-input ecl-text-input--m ecl-search-form__text-input"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+    </div>
+
+    <ecl-button
+      variant="search"
+      label="Search"
+      icon="search"
+      type="submit"
+      class="ecl-search-form__button"
+    />
+  </form>
+</template>
+
+<script>
+import EclButton from "@/components/ecl/EclButton.vue";
+
+/**
+ * ECL Search Form component, see documentation here:
+ *
+ *  https://ec.europa.eu/component-library/ec/components/forms/search-form/usage/
+ *
+ */
+export default {
+  name: "EclSearchForm",
+  components: { EclButton },
+  props: {
+    modelValue: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: "Search",
+    },
+    helpText: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    action: {
+      type: String,
+      required: false,
+      default: ".",
+    },
+    method: {
+      type: String,
+      required: false,
+      default: "GET",
+    },
+    inputName: {
+      type: String,
+      required: false,
+      default: "search",
+    },
+  },
+  emits: ["update:modelValue"],
+};
+</script>
+
+<style scoped></style>
