56c90f10061d62bde851ed0faabf99f1b67b38cb
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-11-11 15:18:23 +0200

    Fix ECL init for default values

--- frontend/src/directives/ECLInit.js
+++ frontend/src/directives/ECLInit.js
@@ -33,13 +33,13 @@ export default {
   // called when the bound element's parent component
   // and all its children are mounted.
   mounted(el, binding) {
-    if (binding.value) {
+    if (binding.value || binding.value === undefined) {
       initComponent(el);
     }
   },
   // called when the parent component is unmounted
   unmounted(el, binding) {
-    if (binding.value) {
+    if (binding.value || binding.value === undefined) {
       destroyComponent(el);
     }
   },
