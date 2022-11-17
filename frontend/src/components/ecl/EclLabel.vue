b0bed07e0f3de455926ad37a045a9d7733d42aab
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-11-17 12:45:22 +0200

    Add ECL Label component

---
+++ frontend/src/components/ecl/EclLabel.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="ecl-label" :class="` ecl-label--${variant}`">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+/**
+ * ECL Label component, see documentation here:
+ *
+ *  https://ec.europa.eu/component-library/ec/components/label/usage/
+ *
+ */
+export default {
+  name: "EclLabel",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    variant: {
+      type: String,
+      required: false,
+      default: "low",
+      validator(value) {
+        return ["low", "medium", "high", "highlight"].includes(value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped></style>
