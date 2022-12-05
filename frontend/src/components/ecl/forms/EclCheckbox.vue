268e499d68b1be4286bab3539b14588833464eae
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-12-05 16:12:25 +0200

    Add ECL Checkbox component

---
+++ frontend/src/components/ecl/forms/EclCheckbox.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="ecl-checkbox">
+    <input
+      v-model="value"
+      :name="inputName"
+      type="checkbox"
+      class="ecl-checkbox__input"
+      :value="inputName"
+    />
+
+    <label class="ecl-checkbox__label">
+      <span class="ecl-checkbox__box">
+        <ecl-icon icon="check" size="m" class="ecl-checkbox__icon" />
+      </span>
+
+      <span class="ecl-checkbox__text">
+        {{ label }}
+      </span>
+    </label>
+
+    <div id="helper-default-1" class="ecl-checkbox__help">
+      {{ helpText }}
+    </div>
+  </div>
+</template>
+
+<script>
+import EclIcon from "@/components/ecl/EclIcon.vue";
+
+/**
+ * ECL Checkbox component, see documentation here:
+ *
+ *  https://ec.europa.eu/component-library/ec/components/forms/checkbox/usage/
+ */
+export default {
+  name: "EclCheckbox",
+  components: { EclIcon },
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    inputName: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    helpText: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/ecl/forms/EclFormGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="ecl-form-group">
+    <label v-if="label" class="ecl-form-label">
+      {{ label }}
+      <span v-if="required" class="ecl-form-label__required">*</span>
+      <span v-else class="ecl-form-label__optional">(optional)</span>
+    </label>
+    <div v-if="helpText" class="ecl-help-block">
+      {{ helpText }}
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EclFormGroup",
+  props: {
+    label: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    helpText: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
