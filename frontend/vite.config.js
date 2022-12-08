bf9c7a212022a17f71d3e0577cd28d9c3ce3aee2
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-12-08 16:11:03 +0200

    Optimize build chunks slightly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,25 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-      path: "path-browserify",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          topology: ["./src/assets/topology.json"],
+          highcharts: [
+            "node_modules/highcharts/highcharts.js",
+            "node_modules/highcharts/highcharts-more.js",
+            "node_modules/highcharts/modules/map.js",
+            "node_modules/highcharts/modules/exporting.js",
+            "node_modules/highcharts/modules/export-data.js",
+            "node_modules/highcharts/modules/offline-exporting.js",
+            "node_modules/highcharts/modules/accessibility.js",
+            "node_modules/highcharts/modules/no-data-to-display.js",
+          ],
+        },
+      },
     },
   },
 });
