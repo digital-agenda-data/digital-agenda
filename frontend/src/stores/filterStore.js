1a076a59a036c4700236248b4372c0520a424c6c
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2022-12-23 09:50:00 +0200

    Reuse FILTER from constants

--- frontend/src/stores/filterStore.js
+++ frontend/src/stores/filterStore.js
@@ -1,24 +1,20 @@
 import { defineStore } from "pinia";
-
-const FILTER_KEYS = [
-  "indicatorGroup",
-  "indicator",
-  "breakdownGroup",
-  "breakdown",
-  "period",
-  "unit",
-  "country",
-];
+import { FILTERS } from "@/lib/constants";
 
 function getInit() {
-  return Object.fromEntries(FILTER_KEYS.map((key) => [key, null]));
+  return Object.fromEntries(FILTERS.map((key) => [key, null]));
 }
+
+/**
+ * Create getters for all filters and axis combinations. I.e:
+ * indicator, indicatorX, indicatorY and so on.
+ */
 
 function makeGetters() {
   const result = {};
 
   for (const suffix of ["", "X", "Y", "Z"]) {
-    for (const key of FILTER_KEYS) {
+    for (const key of FILTERS) {
       result[key + suffix] = function (state) {
         return state[suffix][key];
       };
@@ -28,6 +24,14 @@ function makeGetters() {
   return result;
 }
 
+/**
+ * Store selected filter values in this global store. Even though they are
+ * also (usually) synced with the route query, this store contains the entire
+ * Object from the API instead of only the code.
+ *
+ * There is no two-way binding here, as changing something in this store will
+ * not change the filters or the route!
+ */
 export const useFilterStore = defineStore("filter", {
   state: () => {
     return {
