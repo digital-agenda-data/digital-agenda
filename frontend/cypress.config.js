c2009c8bf78bb90a6710ae8235ad8873d8cb953e
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-01-25 10:10:24 +0200

    Load .env file in cypress config
    Make more packages optional to reduce install time when building for prod

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,7 +1,13 @@
+require("dotenv").config({ path: "../.env" });
+
 const { defineConfig } = require("cypress");
+
+// XXX We don't actually know which one we should use
+// XXX if there are more than one!
+const frontendHost = process.env.FRONTEND_HOST.split(",")[0];
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080",
+    baseUrl: `http://${frontendHost}`,
   },
 });
