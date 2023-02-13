f2bc919f38a79c9e106d9095189429db3c9fc255
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-02-13 10:50:02 +0200

    Add eslint-typescript

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,21 +1,30 @@
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
+const pluginsExtends = [
+  "plugin:vue/vue3-recommended",
+  "eslint:recommended",
+  "@vue/eslint-config-prettier",
+  "plugin:prettier/recommended",
+  "plugin:compat/recommended",
+  "plugin:cypress/recommended",
+];
+
 module.exports = {
   root: true,
   env: {
     node: true,
     browser: true,
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
-    "plugin:prettier/recommended",
-    "plugin:compat/recommended",
-    "plugin:cypress/recommended",
-  ],
+  extends: pluginsExtends,
   parserOptions: {
     ecmaVersion: "latest",
   },
+  overrides: [
+    // Settings onlly for TS
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [...pluginsExtends, "plugin:@typescript-eslint/recommended"],
+    },
+  ],
 };
