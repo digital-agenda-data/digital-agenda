439976c0692409119759ffa055a506adc70371f2
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-02-14 12:32:09 +0200

    Set errors and displayed success messages on feedback

--- frontend/src/views/FeedbackView.vue
+++ frontend/src/views/FeedbackView.vue
@@ -10,6 +10,7 @@
       size="l"
       max-length="200"
       help-text="Provide your email address if you want to be contacted back"
+      :errors="errors.email"
     />
     <ecl-text-area
       v-model="message"
@@ -21,6 +22,7 @@
       min-length="10"
       max-length="10000"
       placeholder-text="Type in your message"
+      :errors="errors.message"
     />
     <ecl-button label="Submit" type="submit" />
   </form>
@@ -31,28 +33,86 @@ import EclButton from "@/components/ecl/EclButton.vue";
 import EclTextArea from "@/components/ecl/forms/EclTextArea.vue";
 import EclTextField from "@/components/ecl/forms/EclTextField.vue";
 import { api } from "@/lib/api";
+import { useMessagesStore } from "@/stores/messagesStore";
+import { mapActions } from "pinia";
 
 export default {
   name: "FeedbackView",
   components: { EclButton, EclTextArea, EclTextField },
   data() {
     return {
+      errors: {
+        email: [],
+        message: [],
+        error: "",
+      },
       email: "",
       message: "",
     };
   },
-  methods: {
-    async submitFeedback() {
-      const previousURL = new URL(
-        this.$router.options.history.state.back,
-        window.location
-      ).href;
-
-      await api.post("/feedback/", {
-        url: previousURL,
+  computed: {
+    postData() {
+      return {
+        url: new URL(
+          this.$router.options.history.state.back ?? "/",
+          window.location
+        ).href,
         email: this.email,
         message: this.message,
-      });
+      };
+    },
+  },
+  methods: {
+    ...mapActions(useMessagesStore, ["addMessage"]),
+    async submitFeedback() {
+      try {
+        this.resetErrors();
+        await api.post("/feedback/", this.postData);
+        this.addMessage({
+          id: "feedback-alert",
+          type: "success",
+          title: "Feedback message sent",
+          description:
+            "Your message has been sent. Thank you for your feedback!",
+        });
+        this.resetForm();
+      } catch (e) {
+        this.setErrors(e);
+        this.addMessage({
+          id: "feedback-alert",
+          type: "error",
+          title: "Unable to send feedback",
+          description: (this.errors.error ?? []).join(","),
+        });
+      }
+    },
+    resetForm() {
+      this.email = "";
+      this.message = "";
+    },
+    resetErrors() {
+      Object.keys(this.errors).forEach((key) => (this.errors[key] = []));
+    },
+    setErrors(error) {
+      const reason = error.response.data;
+
+      if (reason.details) {
+        if (typeof reason.details === "string") {
+          this.errors.error = [reason.details];
+        } else {
+          Object.entries(reason.details).forEach((entry) => {
+            this.errors[entry[0]] = entry[1];
+          });
+        }
+      } else if (reason.detail || reason.error) {
+        this.errors.error = reason.detail || reason.error;
+      } else if (typeof reason === "object") {
+        Object.entries(reason).forEach(([key, messages]) => {
+          this.errors[key] = messages;
+        });
+      } else {
+        this.errors.error = ["Unknown error."];
+      }
     },
   },
 };
@@ -60,7 +120,7 @@ export default {
 
 <style scoped>
 .feedback-form {
-  margin-top: 2rem;
+  margin-top: 1rem;
 }
 
 .feedback-form > * + * {
