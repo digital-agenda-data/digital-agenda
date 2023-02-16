9027fedbde3ca97cd1916440373a44360659e58e
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-02-16 08:53:55 +0200

    Fix urls

--- digital_agenda/site/urls.py
+++ digital_agenda/site/urls.py
@@ -42,12 +42,12 @@ urlpatterns = [
         name="password_reset_done",
     ),
     path(
-        "reset/<uidb64>/<token>/",
+        "admin/reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     path(
-        "reset/done/",
+        "admin/reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
