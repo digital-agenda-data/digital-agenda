2b68326818b244d7c98d4160f0f7be0e45d350c2
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-02-16 08:23:26 +0200

    Add password reset views

--- digital_agenda/site/urls.py
+++ digital_agenda/site/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -30,6 +31,26 @@ urlpatterns = [
     ),
     path("ht/", include("health_check.urls")),
     path("dashboard/", include("django_sql_dashboard.urls")),
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("admin/", admin.site.urls),
     path("", RedirectView.as_view(pattern_name="admin:index")),
 ]
