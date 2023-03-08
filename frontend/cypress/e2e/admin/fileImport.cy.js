792c9043c9e1e240f70c7c7e0b55877c8a000473
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-03-08 11:57:25 +0200

    Add test for invalid data file import

--- frontend/cypress/e2e/admin/fileImport.cy.js
+++ frontend/cypress/e2e/admin/fileImport.cy.js
@@ -34,4 +34,39 @@ describe("Check data file import", () => {
       .get("input[type=submit]")
       .click();
   });
+  it("Create and run invalid data file import", () => {
+    cy.login()
+      // Navigate to the data file import page
+      .get("a")
+      .contains("Data file imports")
+      .click()
+      .get("a")
+      .contains("Add data file import")
+      .click()
+      // Add a new data file import
+      .get("input[type=file]")
+      .selectFile("cypress/fixtures/import_file_invalid.xlsx")
+      .get("textarea[name=description]")
+      .type("Test file invalid import")
+      .get("input[type=submit][value=Save]")
+      .click()
+      // Wait for the task to finish
+      .get("a")
+      .contains("Data file import tasks")
+      .click()
+      .get("tbody tr:first-child td.field-status_display")
+      .contains("FAILURE", { timeout: 10000 })
+      // Navigate to the import config change form
+      .get("tbody tr:first-child td.field-import_file_link a")
+      .click()
+      // Check that only one fact has been imported
+      .get(".field-num_facts a")
+      .contains("0")
+      // Delete the import file
+      .get("a")
+      .contains("Delete")
+      .click()
+      .get("input[type=submit]")
+      .click();
+  });
 });
