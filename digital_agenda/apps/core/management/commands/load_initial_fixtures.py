8c2a18018d7c5fc89546d9cc744bbca917eabcea
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-04-24 16:34:24 +0300

    Add the ability to dump all fixtures back

--- digital_agenda/apps/core/management/commands/load_initial_fixtures.py
+++ digital_agenda/apps/core/management/commands/load_initial_fixtures.py
@@ -2,6 +2,10 @@ from django.core.management import BaseCommand
 from django.core.management import call_command
 from rich.console import Console
 
+from digital_agenda.apps.core.models import *
+from digital_agenda.apps.estat.models import *
+from digital_agenda.apps.charts.models import *
+
 console = Console()
 
 
@@ -10,33 +14,26 @@ class Command(BaseCommand):
 
     FIXTURES = (
         # Order is important
-        "datasources",
-        "indicatorgroups",
-        "indicators",
-        "indicatorgrouplinks",
-        "breakdowngroups",
-        "breakdowns",
-        "breakdowngrouplinks",
-        "datasourcelinks",
-        "units",
-        "periods",
-        "countries",
+        DataSource,
+        IndicatorGroup,
+        Indicator,
+        IndicatorGroupLink,
+        BreakdownGroup,
+        Breakdown,
+        BreakdownGroupLink,
+        IndicatorDataSourceLink,
+        Unit,
+        Period,
+        Country,
         # Imports
-        "geogroups",
-        "importconfigs",
+        GeoGroup,
+        ImportConfig,
         # Charts
-        "chartgroups",
-        "charts",
-    )
-    TEST_FIXTURES = (
-        "test/facts",
-        "test/users",
+        ChartGroup,
+        Chart,
     )
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "--test", action="store_true", help="Also load test fixtures"
-        )
         parser.add_argument(
             "-e",
             "--exclude",
@@ -44,14 +41,34 @@ class Command(BaseCommand):
             default=[],
             help="Exclude fixtures from the list",
         )
+        parser.add_argument(
+            "--dump",
+            action="store_true",
+            default=False,
+            help="Dump the data back to the fixtures instead of loading it in the DB",
+        )
 
-    def handle(self, *args, exclude, test=False, **options):
-        fixtures = self.FIXTURES
-        if test:
-            fixtures += self.TEST_FIXTURES
+    def handle(self, *args, exclude, dump=False, **options):
+        for model in self.FIXTURES:
+            opt = model._meta
+            name = opt.model_name
+            path = settings.INITIAL_FIXTURES_DIR / f"{name}.json"
 
-        for name in fixtures:
             if name in exclude:
                 continue
-            console.print(f"Loading from fixture: '{name}'")
-            call_command("loaddata", f"{name}.json")
+
+            if dump:
+                console.print(f"Dumping fixtures: {path}")
+                call_command(
+                    "dumpdata",
+                    "--indent",
+                    "4",
+                    "-o",
+                    str(path),
+                    "--natural-foreign",
+                    "--natural-primary",
+                    f"{opt.app_label}.{name}",
+                )
+            else:
+                console.print(f"Loading from fixture: '{name}'")
+                call_command("loaddata", name)
