f286b3720c92d156154b31af1bfa380def0035c7
alexkiro <1538458+alexkiro@users.noreply.github.com>
2023-09-19 09:36:11 +0300

    Fix data source export (#28)

--- digital_agenda/settings/base.py
+++ digital_agenda/settings/base.py
@@ -255,7 +255,7 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
-        "rest_framework_csv.renderers.CSVRenderer",
+        "digital_agenda.common.renderers.CustomCSVRenderer",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
