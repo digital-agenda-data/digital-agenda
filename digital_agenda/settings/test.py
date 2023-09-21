0c7acf6cb5227e5676c335712a65ccc91074f2d2
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-09-21 10:13:15 +0300

    Add missing episodes

--- digital_agenda/settings/test.py
+++ digital_agenda/settings/test.py
@@ -13,5 +13,5 @@ BETAMAX_FIXTURES = str(BASE_DIR / "fixtures" / "test_betamax")
 
 with Betamax.configure() as config:
     config.cassette_library_dir = BETAMAX_FIXTURES
-    config.default_cassette_options["record_mode"] = "new_episodes"
+    config.default_cassette_options["record_mode"] = "once"
     config.default_cassette_options["preserve_exact_body_bytes"] = "all"
