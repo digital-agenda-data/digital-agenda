8bf0498079d4de2acab234164820a446de33ca9b
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-10-11 09:07:28 +0300

    Add disable_inactive_accounts to cron

--- digital_agenda/apps/accounts/jobs.py
+++ digital_agenda/apps/accounts/jobs.py
@@ -17,3 +17,9 @@ def clear_sessions():
 def clear_cas_sessions():
     logger.info("Clearing CAS sessions")
     call_command("django_cas_ng_clean_sessions")
+
+
+@cron("20 1 * * *")
+def disable_inactive_accounts():
+    logger.info("Disabling inactive accounts")
+    call_command("disable_inactive_accounts")
