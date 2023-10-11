950dc248973212ab2b3bf41346a295dd975c0203
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-10-11 09:32:47 +0300

    Add tests for the disable accounts script

--- digital_agenda/apps/accounts/tests/test_users.py
+++ digital_agenda/apps/accounts/tests/test_users.py
@@ -1,17 +1,21 @@
+from datetime import timedelta
+
 import pytest
 
 from django.contrib.auth import get_user_model
-
+from django.core.management import call_command
+from django.utils import timezone
 
 pytestmark = [pytest.mark.django_db]
 
+User = get_user_model()  # noqa
+
 
 def test_create_user():
-    User = get_user_model()  # noqa
-    user = User.objects.create_user(email="normal@user.com", password="foo")
-    assert user.email == "normal@user.com"
+    user = User.objects.create_user(email="normal@example.com", password="foo")
+    assert user.email == "normal@example.com"
     assert user.is_active
-    assert not user.is_staff
+    assert user.is_staff
     assert not user.is_superuser
 
     with pytest.raises(AttributeError):
@@ -24,19 +28,48 @@ def test_create_user():
         User.objects.create_user(email="")
 
     with pytest.raises(TypeError):
-        User.objects.create_user(email="normal@user.com")
+        User.objects.create_user(email="normal@example.com")
 
     with pytest.raises(ValueError):
         User.objects.create_user(email="", password="foo")
 
     with pytest.raises(ValueError):
-        User.objects.create_user(email="normal@user.com", password="")
+        User.objects.create_user(email="normal@example.com", password="")
 
 
 def test_create_superuser():
-    User = get_user_model()  # noqa
-    user = User.objects.create_superuser(email="super@user.com", password="foo")
-    assert user.email == "super@user.com"
+    user = User.objects.create_superuser(email="super@example.com", password="foo")
+    assert user.email == "super@example.com"
     assert user.is_active
     assert user.is_staff
     assert user.is_superuser
+
+
+def test_disable_inactive_user():
+    user1 = User.objects.create_superuser(email="super1@example.com", password="foo")
+    user2 = User.objects.create_superuser(email="super2@example.com", password="foo")
+
+    user1.last_login = timezone.now() - timedelta(days=179)
+    user1.save()
+    user2.last_login = timezone.now() - timedelta(days=180)
+    user2.save()
+
+    with pytest.raises(AssertionError):
+        call_command("disable_inactive_accounts", "--days=-1")
+    user1.refresh_from_db()
+    assert user1.is_active
+    user2.refresh_from_db()
+    assert user2.is_active
+
+    call_command("disable_inactive_accounts", "--days=0")
+    user1.refresh_from_db()
+    assert user1.is_active
+    user2.refresh_from_db()
+    assert user2.is_active
+
+    call_command("disable_inactive_accounts", "--days=180")
+
+    user1.refresh_from_db()
+    assert user1.is_active
+    user2.refresh_from_db()
+    assert not user2.is_active
