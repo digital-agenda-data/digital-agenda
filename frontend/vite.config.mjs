1e9675849fdda05270e0e41b470a339b7d63c053
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-02-20 11:51:13 +0200

    Fix style issue

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,13 +4,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import legacy from "@vitejs/plugin-legacy";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy(),
-  ],
+  plugins: [vue(), legacy()],
   server: {
     port: 8080,
   },
