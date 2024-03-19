7b629bdf37f7e22cc102cc5903fe9fe3c9dfdb19
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-03-19 13:51:28 +0200

    Add tests for hiding data points with extra notes

--- frontend/cypress/e2e/charts/11.test-extra-notes/1.test-spline-compare-indicators-notes.cy.js
+++ frontend/cypress/e2e/charts/11.test-extra-notes/1.test-spline-compare-indicators-notes.cy.js
@@ -19,4 +19,25 @@ describeResponsive("Check Chart", () => {
       xAxis: ["DESI period: 2018"],
     });
   });
+  it("Check Chart Hidden Points from extra notes", () => {
+    cy.navigateToChart(
+      "Test Extra Notes",
+      "Test Spline Compare Indicators Notes",
+    );
+    cy.checkChart({
+      filters: {
+        indicatorX: "e-Invoices",
+        indicatorY: "Access to e-health records",
+      },
+      point: "DESI period: 2019 (data from 2018), 24.7944. e-Invoices.",
+      // 3 points from e-Invoices instead of 5 (because 2 are hidden via extra notes)
+      // 1 point from Access to e-health records
+      pointNr: 4,
+      tooltip: [
+        "e-Invoices",
+        "24.79% of enterprises",
+        "DESI period: 2019 (data from 2018)",
+      ],
+    });
+  });
 });
