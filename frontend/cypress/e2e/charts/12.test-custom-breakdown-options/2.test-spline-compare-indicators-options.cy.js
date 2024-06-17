1f1d808a89b676a4672abc961db0d038dd249477
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-06-17 11:39:00 +0300

    Add test to check some basic custom options

---
+++ frontend/cypress/e2e/charts/12.test-custom-breakdown-options/2.test-spline-compare-indicators-options.cy.js
@@ -0,0 +1,24 @@
+import describeResponsive from "../../../support/describeResponsive";
+
+describeResponsive("Check Chart", () => {
+  it("Check Chart", () => {
+    cy.navigateToChart(
+      "Test Custom Breakdown Options",
+      "Test Spline Compare Indicators",
+    );
+    cy.checkChart({
+      filters: {
+        breakdownX: "Non-nationals",
+        breakdownY: "Nationals",
+      },
+      title: [
+        "playing or downloading games, images, films or music",
+        "European Union",
+      ],
+      point: "Year: 2010, 23.53. Games, images, films or music.",
+      tooltip: ["Non-nationals", "23.53% of individuals"],
+    });
+    // Check data labels
+    cy.get(".highcharts-data-label").contains("23.53");
+  });
+});
