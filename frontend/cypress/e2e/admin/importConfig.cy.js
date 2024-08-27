65ac8114858108e9cb15c5a6806c53f59057254b
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-08-27 12:16:46 +0300

    Add tests for ESTAT imports of the new fields

--- frontend/cypress/e2e/admin/importConfig.cy.js
+++ frontend/cypress/e2e/admin/importConfig.cy.js
@@ -9,6 +9,9 @@ describe("Check import configuration", () => {
     cy.get("input[name=title]").type("Test import config");
     cy.get("input[name=indicator]").type("indic_is");
     cy.get("input[name=breakdown]").type("hhtyp");
+    cy.get("input[name=reference_period]").type("time");
+    cy.get("input[name=remarks]").type("test remarks");
+    cy.get("input[name=remarks_is_surrogate]").click();
     cy.get("input[name=period_start]").type("2019");
     cy.get("input[name=period_end]").type("2019");
     cy.get("#id_filters textarea.ace_text-input").clear({ force: true });
@@ -21,7 +24,7 @@ describe("Check import configuration", () => {
     );
     cy.get("#id_mappings textarea.ace_text-input").clear({ force: true });
     cy.get("#id_mappings textarea.ace_text-input").type(
-      '{"breakdown": {"total": "hh_total"}, "country": {"EU27_2020": "EU"}}',
+      '{"breakdown": {"total": "hh_total"}, "country": {"EU27_2020": "EU"}, "reference_period": {"2019": "2000"}}',
       {
         force: true,
         parseSpecialCharSequences: false,
@@ -38,9 +41,22 @@ describe("Check import configuration", () => {
     });
     // Navigate to the import config change form
     cy.get("tbody tr:first-child td.field-import_config_link a").click();
-    // Check that only one fact has been imported
-    cy.get(".field-num_facts a").contains("1");
+    // Check that only one fact has been imported, and open the fact details
+    cy.get(".field-num_facts a").contains("1").click();
+    cy.get("tbody tr:first-child th.field-indicator a").click();
+
+    // Check imported values
+    cy.get("[name=value]").should("have.value", "87.53");
+    cy.get(".field-indicator").contains("[h_broad]");
+    cy.get(".field-breakdown").contains("[hh_total]");
+    cy.get(".field-unit").contains("[pc_hh]");
+    cy.get(".field-country").contains("[EU]");
+    cy.get(".field-period").contains("[2019]");
+    cy.get("[name=reference_period]").should("have.value", "2000");
+    cy.get("[name=remarks]").should("have.value", "test remarks");
+
     // Delete the import config
+    cy.get(".field-import_config .view-related").click();
     cy.get("a").contains("Delete").click();
     cy.get("input[type=submit]").click();
   });
