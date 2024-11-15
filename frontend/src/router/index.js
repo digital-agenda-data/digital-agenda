26ac761e57be92a5b8b5a31a124d77db362bc547
alexkiro <1538458+alexkiro@users.noreply.github.com>
2024-11-15 16:03:58 +0200

    Add title for home page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -35,6 +35,9 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        title: "Digital Decade DESI visualisation tool",
+      },
     },
     {
       path: "/about",
