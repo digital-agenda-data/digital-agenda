3b5ab921bec0f64544d5fa0dbae54e058064551f
alexkiro <1538458+alexkiro@users.noreply.github.com>
2024-11-18 14:56:33 +0200

    Unmount the WebTools when the Vue component is unmounted

--- frontend/src/directives/ECWebTool.js
+++ frontend/src/directives/ECWebTool.js
@@ -12,19 +12,29 @@
  *     }"
  *   />
  */
-export default (el, binding) => {
-  const render = () => {
-    if (!window.$wt.exists(binding.value.service || binding.value.utility)) {
-      window.$wt.render(el, binding.value);
+export default {
+  mounted(el, binding) {
+    if (window.$wt) {
+      render(el, binding);
+    } else {
+      window.addEventListener("wtReady", () => render(el, binding), {
+        once: true,
+        capture: false,
+      });
     }
-  };
-
-  if (window.$wt) {
-    render();
-  } else {
-    window.addEventListener("wtReady", render, {
-      once: true,
-      capture: false,
-    });
-  }
+  },
+  unmounted(el) {
+    const snippet = el.querySelector("script");
+    for (const key in window.$wt.components) {
+      if (window.$wt.components[key] === snippet) {
+        delete window.$wt.components[key];
+      }
+    }
+  },
 };
+
+function render(el, binding) {
+  if (!window.$wt.exists(binding.value.service || binding.value.utility)) {
+    window.$wt.render(el, binding.value);
+  }
+}
