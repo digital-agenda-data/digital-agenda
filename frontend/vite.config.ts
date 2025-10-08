3b60e9231aaac3a10fc51eb594e865728b7f32e6
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-10-08 15:57:52 +0300

    More minor updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   server: {
     port: 8080,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
@@ -38,4 +35,4 @@ export default defineConfig({
   test: {
     environment: "jsdom",
   },
-})
+});
