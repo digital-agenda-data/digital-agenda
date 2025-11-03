9e6ec2b1d116eb6cc4b014838dce819e5824e24a
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-11-03 10:20:19 +0200

    Fix color contrast issue

--- frontend/src/components/CardNav.vue
+++ frontend/src/components/CardNav.vue
@@ -93,7 +93,7 @@ export default {
 .card-nav-item-active,
 .card-nav-item:hover {
   .ecl-card {
-    background-color: var(--ecl-color-neutral-60);
+    background-color: var(--ecl-color-neutral-40);
   }
 }
 </style>
