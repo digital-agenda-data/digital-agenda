f3dc98f6953e9c22f8aacb588ae9d5fdd0e0f92a
alexkiro <1538458+alexkiro@users.noreply.github.com>
2025-11-13 11:09:44 +0200

    Add logging for uncaught exceptions

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -6,3 +6,15 @@ import "cypress-network-idle";
 import verifyDownloads from "cy-verify-downloads";
 
 verifyDownloads.addCustomCommand();
+
+Cypress.on("uncaught:exception", (err) => {
+  Cypress.log({
+    name: "Uncaught Exception",
+    message: err.message,
+    consoleProps: () => ({
+      error: err,
+    }),
+  });
+
+  console.error("Uncaught Exception:", err);
+});
