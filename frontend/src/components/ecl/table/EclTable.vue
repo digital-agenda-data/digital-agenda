c121f4a325c85880dbb48854b81c985144fed582
alexkiro <1538458+alexkiro@users.noreply.github.com>
2026-04-08 11:58:22 +0300

    Make ECL table reusuable

---
+++ frontend/src/components/ecl/table/EclTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <table class="ecl-table" :class="{ 'ecl-table--zebra': zebra }">
+    <slot></slot>
+  </table>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "EclTable",
+  props: {
+    zebra: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+});
+</script>
---
+++ frontend/src/components/ecl/table/EclTd.vue
@@ -0,0 +1,20 @@
+<template>
+  <td class="ecl-table__cell" :data-ecl-table-header="header">
+    <slot></slot>
+  </td>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "EclTd",
+  props: {
+    header: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+});
+</script>
