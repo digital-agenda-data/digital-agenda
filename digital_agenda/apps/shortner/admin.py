cf40b54be0aaa055e3345effd93e55a42a5607a7
alexkiro <1538458+alexkiro@users.noreply.github.com>
2026-05-26 11:27:47 +0300

    Add ShortURL Admin

--- digital_agenda/apps/shortner/admin.py
+++ digital_agenda/apps/shortner/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+from digital_agenda.apps.shortner.models import ShortURL
+
+
+@admin.register(ShortURL)
+class ShortURLAdmin(ImportExportModelAdmin):
+    list_display = ("id", "chart", "query_arguments", "created_at")
