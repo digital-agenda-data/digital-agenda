de24efe96e2224620875467f37e459fc3cb1a027
alexkiro <1538458+alexkiro@users.noreply.github.com>
2026-05-27 15:32:12 +0300

    Re-enable tests for indicator group export links

--- frontend/cypress/e2e/indicators.cy.js
+++ frontend/cypress/e2e/indicators.cy.js
@@ -46,27 +46,29 @@ describeResponsive("Check Indicators Page", () => {
       .contains("Consult the list of indicators, their definition and sources")
       .click();
 
+    cy.waitForNetworkIdle(1000, { log: false });
+
     // Check exporting data for an indicator group
-    // cy.get("thead a")
-    //   .contains("data")
-    //   .parent("a")
-    //   .downloadLink()
-    //   .should("contain", "period,country,indicator,breakdown,unit,value,flags");
-    //
-    // // Check exporting values for an indicator group
-    // for (const label of [
-    //   "indicators",
-    //   "data sources",
-    //   "countries",
-    //   "breakdowns",
-    //   "units",
-    // ]) {
-    //   cy.get("thead a")
-    //     .contains(label)
-    //     .parent("a")
-    //     .downloadLink()
-    //     .should("contain", "code,label,alt_label,definition");
-    // }
+    cy.get("h3 + div a")
+      .contains("data")
+      .parent("a")
+      .downloadLink()
+      .should("contain", "period,country,indicator,breakdown,unit,value,flags");
+
+    // Check exporting values for an indicator group
+    for (const label of [
+      "indicators",
+      "data sources",
+      "countries",
+      "breakdowns",
+      "units",
+    ]) {
+      cy.get("h3 + div a")
+        .contains(label)
+        .parent("a")
+        .downloadLink()
+        .should("contain", "code,label,alt_label,definition");
+    }
 
     // Check exporting values for an indicator
     cy.get('[aria-controls="indicator-group-table-desi_ind"]').click();
